a1970c0853c6be9984644fc95ea8808f6eb2a793
Ivan Tkachuk <tkachukivan93@gmail.com>
2019-08-11 17:57:50 +0200

    unsubscribe from price change

--- src/app/pages/product-details/edit-product-modal/edit-price/edit-price.component.ts
+++ src/app/pages/product-details/edit-product-modal/edit-price/edit-price.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ChangeDetectionStrategy, HostBinding, Input } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, HostBinding, Input, OnDestroy } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-edit-price',
@@ -7,18 +8,23 @@ import { FormGroup } from '@angular/forms';
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class EditPriceComponent implements OnInit {
+export class EditPriceComponent implements OnInit, OnDestroy {
   @HostBinding('attr.class') cssClass = 'product-edit-modal-section';
   @Input() parent: FormGroup;
+  private priceSub: Subscription;
   constructor() { }
 
   ngOnInit() {
-    this.price.valueChanges
+    this.priceSub = this.price.valueChanges
       .subscribe((value) => {
         if (value < 0) {
           this.price.setValue(null);
         }
       });
+  }
+
+  ngOnDestroy() {
+    this.priceSub.unsubscribe();
   }
 
   get price() {
