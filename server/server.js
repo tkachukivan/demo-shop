8d49b697b828cfb908bfa7770abf926572bd4bce
Ivan Tkachuk <tkachukivan93@gmail.com>
2019-08-11 18:56:48 +0200

    add 404 error handling

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router(__dirname + '/db.json');
-const middlewares = jsonServer.defaults();
+const path = require('path');
 const bodyParser = require('body-parser');
 const authRoutes = require('./auth-routes');
 
-const PORT = process.env.PORT || 3000;
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, './db.json'));
+const middlewares = jsonServer.defaults();
 
+const PORT = process.env.PORT || 3000;
 server.use(middlewares);
 
 server.use(bodyParser.json());
@@ -14,6 +15,11 @@ server.post('/api/login', authRoutes.login);
 server.post('/api/logout', authRoutes.logout);
 server.use('/api', authRoutes.isAuthorized);
 server.use('/api', router);
+
+// let frontend router deal with 404 errors
+server.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, './../public/index.html'));
+});
 
 server.listen(PORT, function() {
   console.log('JSON Server is running on port ' + PORT);
